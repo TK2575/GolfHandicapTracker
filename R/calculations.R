5a8e2de31c09c4e1d5d1c060a47e1d48a0efb6cd
Tom Kain <TK2575@users.noreply.github.com>
2018-06-30 20:54:52 -0700

    Starting nine hole round work, fixed some incorrect nine hold rating data

--- R/calculations.R
+++ R/calculations.R
@@ -4,31 +4,36 @@ transform_inputs <- function(input_data) {
     validate_inputs %>%
     unique %>%
     dplyr::mutate(dt = lubridate::mdy(date)) %>%
-    dplyr::select(-hndcp_diff, -date) %>%
+    dplyr::select(-date) %>%
     dplyr::rename(date = dt) %>%
     dplyr::mutate(quarter = lubridate::floor_date(date, "quarter")) %>%
-    dplyr::arrange(date) %>%
+    dplyr::arrange(date)
+
+  if ("nine_hole_round" %in% names(result)) {
+    nine_hole_rounds <- result %>%
+      subset(nine_hole_round==T) %>%
+      compute_nine_hole_rounds()
+
+    eighteen_hole_rounds <- result %>%
+      subset(nine_hole_round==F)
+
+    result <- dplyr::bind_rows(eighteen_hole_rounds, nine_hole_rounds) %>%
+      dplyr::arrange(date)
+  }
+
+  result %>%
     dplyr::mutate(over_under = compute_over_under(score, par),
                   hndcp_diff = compute_handicap_differential(score, rating, slope)) %>%
     dplyr::mutate(fir = fairways_hit/fairways * 100) %>%
     dplyr::mutate(gir = greens_in_reg/18 * 100) %>%
     dplyr::mutate(pph = putts/18)
 
-    diffs <- result %>% select(hndcp_diff) %>% dplyr::pull()
-    purrr::map(
-     .x=c(1:length(diff)),
-     .f=compute_handicap_index,
-     handicap_differentials=diff)
+  result <- add_handicap_indexes(result)
 
-    hndcp_index <-
-      purrr::map(c(1:length(diffs)),compute_handicap_index,handicap_differentials = diffs) %>%
-      unlist()
-
-    result %>%
-      tibble::add_column(hndcp_index) %>%
-      dplyr::mutate(course_handicap = compute_course_handicap(hndcp_index, slope)) %>%
-      dplyr::mutate(net_score = compute_net_score(score, course_handicap)) %>%
-      dplyr::mutate(net_over_under = as.integer(round(net_score - par)))
+  result %>%
+    dplyr::mutate(net_score = compute_net_score(score, course_handicap)) %>%
+    dplyr::mutate(net_over_under = as.integer(round(net_score - par))) %>%
+    select(-hndcp_diff)
 }
 
 compute_over_under <- function(score, par) {
@@ -123,4 +128,28 @@ validate_inputs <- function(input_data) {
     warning("Missing or Empty Optional column(s)")
   }
 
+}
+
+add_handicap_indexes <- function(df) {
+  diffs <- df %>% select(hndcp_diff) %>% dplyr::pull()
+  purrr::map(
+    .x=c(1:length(diff)),
+    .f=compute_handicap_index,
+    handicap_differentials=diff)
+
+  hndcp_index <-
+    purrr::map(c(1:length(diffs)),compute_handicap_index,handicap_differentials = diffs) %>%
+    unlist()
+
+  df %>%
+    tibble::add_column(hndcp_index) %>%
+    dplyr::mutate(course_handicap = compute_course_handicap(hndcp_index, slope))
+}
+
+compute_nine_hole_rounds <- function(df) {
+  if (nrow(df) %% 2 != 0) {
+    df <- df[1:nrow(df)-1,]
+  }
+
+# TODO - from each odd numbered row, "sum" up it and the lead (next even numbered) row
 }
